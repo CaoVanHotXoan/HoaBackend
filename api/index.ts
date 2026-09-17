2c7172a3f1f96dc4394d59f679e37290ebdbc4b4
caovanhotxoan <sinhvienxoan@gmail.com>
2026-09-17 14:30:06 +0700

    ok

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import serverless from 'serverless-http';
+
+let server: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp);
+}
+
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    server = await bootstrap();
+  }
+  return server(req, res);
+}
