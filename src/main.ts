a8f2de478158f3a0b15e0ddec80bd442a3b33b56
caovanhotxoan <sinhvienxoan@gmail.com>
2026-09-17 13:53:14 +0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   logger.log(`Application is running on: http://localhost:${port}/api`);
   logger.log(
     `Swagger Documentation is running on: http://localhost:${port}/api/docs`,
