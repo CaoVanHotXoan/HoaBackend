44d6af7eb7bc452b2279eb945c2556cab25467a3
caovanhotxoan <sinhvienxoan@gmail.com>
2026-09-17 23:05:28 +0700

    ok

--- dist/src/user-actions/user-actions.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { DatabaseService } from '../database/database.service';
-export declare class UserActionsService {
-    private readonly databaseService;
-    private readonly logger;
-    constructor(databaseService: DatabaseService);
-    logAction(userId: number, actionType: string, productId?: number, searchQuery?: string): Promise<{
-        success: boolean;
-    }>;
-}
